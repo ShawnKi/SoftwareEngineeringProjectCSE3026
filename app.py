234d4708fb40a8d4601a208b4d879bbb313a8de9
Shawn <89417917+ShawnKi@users.noreply.github.com>
2024-02-12 19:38:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ templates_dir = os.path.join(dir_path, 'test1', 'templates')
 
 # Create the Flask application and specify the templates directory
 app = Flask(__name__, template_folder=templates_dir)
-
+print("Template folder (relative):", app.template_folder)
+print("Template folder (absolute):", os.path.join(app.root_path, app.template_folder))
 @app.route('/')
 def index():
     return render_template('index.html')
