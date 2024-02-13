be7ba20cb650c470513e71f40e01885dc078fb81
Shawn <89417917+ShawnKi@users.noreply.github.com>
2024-02-12 19:19:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/templates')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/templates')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-
-@app.route('/templates')
+@app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
 
-
-@app.route('/templates')
+@app.route('/login')
 def login():
     return render_template('login.html')
 
