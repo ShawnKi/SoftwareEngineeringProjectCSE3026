03df53a48781b08b20ccb74c756d6b28c8fd5ea3
Shawn <89417917+ShawnKi@users.noreply.github.com>
2024-02-12 20:35:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template
 import os
 
-# Get the absolute path of the directory where app.py is located
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-# Define the templates directory
-templates_dir = os.path.join(dir_path, 'test1', 'templates')
-
 # Create the Flask application and specify the templates directory
-app = Flask(__name__, template_folder=templates_dir)
+app = Flask(__name__, template_folder='templates')
 
 print("Template folder (relative):", app.template_folder)
-print("Template folder (absolute):", os.path.join(app.root_path, app.template_folder))
 
 @app.route('/')
 def index():
